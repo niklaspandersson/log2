96b5e7617310d55b219fbe734718df1162fda15a
Niklas Andersson <niklas@niklaspandersson.se>
2019-02-26 21:13:53 +0100

    stated calendar-view for better overview of a journal

--- src/frontend/views/JournalView.tsx
+++ src/frontend/views/JournalView.tsx
@@ -32,10 +32,67 @@ function PostListItem(post:Post&{select: (post:Post) => void}) {
             </li>
 }
 
+interface DayOfMonthProps {
+    date:Moment;
+    prevMonth:boolean;
+    nextMonth:boolean;
+    distance:number;
+}
+
+function DayOfMonth(props:DayOfMonthProps) {
+    return  <div className={classnames({day: true, future: props.distance > 0, 'prev-month': props.prevMonth, 'next-month': props.nextMonth, current: props.distance === 0 })}>
+                <div className="date">{props.date.date()}</div>
+            </div>
+}
+function getDaySpan(date:Moment) : [Moment, number] {
+
+    let firstOfMonth = date.startOf('month');
+    let wd = firstOfMonth.isoWeekday();
+    let firstOfCal = firstOfMonth.clone().subtract(wd-1, 'days');
+
+    let lastOfMonth = date.endOf('month');
+    let wdl = lastOfMonth.isoWeekday();
+    let lastOfCal = lastOfMonth.clone().add(7 - wdl, 'days');
+
+    let diff = Math.round(lastOfCal.diff(firstOfCal, 'days', true));
+    return [firstOfCal, diff];
+}
+
+function getDays(date:Moment, posts:Post[]) {
+    let today = moment(0, "HH");
+    let [first, numDays] = getDaySpan(date);
+    let month = date.month();
+
+    let days = [];
+    for(let i=0; i<numDays;++i) {
+        let date = first.clone().add(i, 'days');
+        days.push(<DayOfMonth key={date.format()} date={date} distance={date.diff(today, 'days')} prevMonth={date.month() < today.month()} nextMonth={date.month() > today.month()} />)
+    }
+
+    return days;
+}
+
+interface MonthProps {
+    date:Moment;
+    posts: Post[];
+}
+function Month(props:MonthProps) {
+    let days = getDays(props.date, props.posts);
+
+    return  <section className="month">
+                <header>{props.date.format("MMMM").toLocaleLowerCase()}</header>
+                <div className="calendar">
+                    {days}
+                </div>
+            </section>
+}
+
 export default function JournalView(props:JournalViewProps) {
+    let now = moment();
     let posts = props.posts && props.posts.map(post => <PostListItem key={post._id} {...post} select={props.onSelectPost} />)
     return  <div className={classnames("journal", props.className)}>
                 { /*!hasToday && createPostButton() */ }
+                <Month date={now} posts={props.posts.filter(p => moment(p.time).month() === now.month())} />
                 <ul>
                     {posts}
                 </ul>
